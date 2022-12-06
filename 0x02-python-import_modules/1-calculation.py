9e47d86420ed3b1601a0a13d1bddf909707eeb44
zimura <chidimmaibezim0@gmail.com>
2022-12-06 12:35:30 -0800

    Hidden

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """print the sum, difference, multiple and quotient of 10 and 5."""
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
@@ -11,3 +11,4 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     for i in range(len(sys.argv) -1):
         total += int(sys.argv[i + 1])
     print("{}".format(total))
+
